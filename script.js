be24db3a61c75cf03d09ed7cb676b79e8aa8ac8f
Andrew Fillenwarth <andyf016@gmail.com>
2019-12-19 13:32:42 -0500

    fix a couple win conditions

--- script.js
+++ script.js
@@ -2,18 +2,18 @@ let gameActive = false;
 let activePlayer = 0;
 let gameBoard = [];
 let playerColor = [];
-playerColor[1] = "red";
-playerColor[2] = "blue";
+playerColor[1] = "blue";
+playerColor[2] = "red";
 
-let beginGame = function(){
+let beginGame = function () {
     if (gameActive == true) return false;
     gameActive = true;
 
-    for (row = 0; row<=5; row++){
+    for (row = 0; row <= 5; row++) {
         gameBoard[row] = [];
-        for (col = 0; col<=6; col++){
-        gameBoard[row][col] = 0;
-        }console.log("Begin!")
+        for (col = 0; col <= 6; col++) {
+            gameBoard[row][col] = 0;
+        } console.log("Begin!")
     }
 
     drawBoard();
@@ -22,23 +22,23 @@ let beginGame = function(){
 }
 
 
-let drawBoard = function(){
+let drawBoard = function () {
     console.log("Draw")
     checkForWin();
-    for(col = 0; col<=6; col++){
-        for( row = 0; row <=5; row++){
-            document.getElementById('square_'+row+'_'+col).innerHTML="<span class = 'piece player" +gameBoard[row][col]+"'></span>";
+    for (col = 0; col <= 6; col++) {
+        for (row = 0; row <= 5; row++) {
+            document.getElementById('square_' + row + '_' + col).innerHTML = "<span class = 'piece player" + gameBoard[row][col] + "'></span>";
         }
     }
 }
 
-let checkForWin = function(){
+let checkForWin = function () {
     console.log("check")
-    for (i=1; i<=2; i++){
-        for(col = 0; col<=3; col++){
-            for(row = 0; row <=5; row++){
-                if(gameBoard[row][col] == i){
-                    if((gameBoard[row][col+1]==i) && (gameBoard[row][col+2]==i) && (gameBoard[row][col+3]==i)){
+    for (i = 1; i <= 2; i++) {                       //Check for left to right win condition
+        for (col = 0; col <= 3; col++) {
+            for (row = 0; row <= 5; row++) {
+                if (gameBoard[row][col] == i) {
+                    if ((gameBoard[row][col + 1] == i) && (gameBoard[row][col + 2] == i) && (gameBoard[row][col + 3] == i)) {
                         endGame(i);
                         return true;
                     }
@@ -47,11 +47,11 @@ let checkForWin = function(){
         }
     }
 
-    for (i=1; i<=2; i++){
-        for (col = 0; col <=2; col++){
-            for(row = 0; row<=2; row++){
-                if (gameBoard[row][col]==i){
-                    if((gameBoard[row+1][col]==i)&&(gameBoard[row+2][col]==i) && (gameBoard[row+3][col] == i)){
+    for (i = 1; i <= 2; i++) {                       //Check for top to bottom win condition
+        for (col = 0; col <= 6; col++) {
+            for (row = 0; row <= 2; row++) {
+                if (gameBoard[row][col] == i) {
+                    if ((gameBoard[row + 1][col] == i) && (gameBoard[row + 2][col] == i) && (gameBoard[row + 3][col] == i)) {
                         endGame(i);
                         return true;
                     }
@@ -59,11 +59,11 @@ let checkForWin = function(){
             }
         }
     }
-    for (i=1; i<=2; i++){
-        for(col = 0; col <=3; col++){
-            for (row = 0; row <=2; row++){
-                if(gameBoard[row][col] ==i){
-                    if((gameboard[row +1][col+1] == i) && (gameBoard[row+2][col+2]==i) && (gameBoard[row+3][col+3]==i)){
+    for (i = 1; i <= 2; i++) {                       //Check for diagonal down win condition
+        for (col = 0; col <= 3; col++) {
+            for (row = 0; row <= 2; row++) {
+                if (gameBoard[row][col] == i) {
+                    if ((gameBoard[row + 1][col + 1] == i) && (gameBoard[row + 2][col + 2] == i) && (gameBoard[row + 3][col + 3] == i)) {
                         endGame(i);
                         return true;
                     }
@@ -71,11 +71,11 @@ let checkForWin = function(){
             }
         }
     }
-    for (i=1; i<=2; i++){
-        for(col = 0; col <=3; col++){
-            for(row = 3; row<=5; row++){
-                if(gameBoard[row][col]==i){
-                    if((gameBoard[row-1][col+1]==i) && (gameBoard[row-2][row+2] == i) && (gameBoard[row-3][row+3]==i)){
+    for (i = 1; i <= 2; i++) {                   //Check for diagonal up win condition
+        for (col = 0; col <= 3; col++) {
+            for (row = 3; row <= 5; row++) {
+                if (gameBoard[row][col] == i) {
+                    if ((gameBoard[row - 1][col + 1] == i) && (gameBoard[row - 2][row + 2] == i) && (gameBoard[row - 3][row + 3] == i)) {
                         endGame(i);
                         return true;
                     }
@@ -85,26 +85,28 @@ let checkForWin = function(){
     }
 }
 
-let endGame = function(winningPlayer){
+let endGame = function(winningPlayer) {        //End Game function
+    console.log("end")
     gameActive = false;
-    document.getElementById('game_info').innerHTML = "Winner: " + winningPlayer; 
+    document.getElementById('game_info').innerHTML = "Winner: " + winningPlayer;
+    
 }
 
-let setUpTurn = function(){
+let setUpTurn = function () {
     console.log("setUp")
-    if(gameActive){
-        document.getElementById('game_info').innerHTML = "Current Player: Player " + activePlayer + " <span class='player"+activePlayer+"'>" + playerColor[activePlayer] + "</span>";
+    if (gameActive) {
+        document.getElementById('game_info').innerHTML = "Current Player: Player " + activePlayer + " <span class='player" + activePlayer + "'>" + playerColor[activePlayer] + "</span>";
     }
 }
 
-let drop = function(col){
+let drop = function (col) {                 //Add peice to the lowest possible column 
     console.log("Drop!")
-    for (row = 5; row>=0; row--){
-        if(gameBoard[row][col]==0){
+    for (row = 5; row >= 0; row--) {
+        if (gameBoard[row][col] == 0) {
             gameBoard[row][col] = activePlayer;
             drawBoard();
-            if(activePlayer == 1){
-                activePlayer= 2; 
+            if (activePlayer == 1) {
+                activePlayer = 2;
             } else {
                 activePlayer = 1;
             }
