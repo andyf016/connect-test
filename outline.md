50894cb6c806258b3a7abdc0d315f020fede130f
Andrew Fillenwarth <andyf016@gmail.com>
2019-12-20 15:31:00 -0500

    fix win conditions add tie condition

---
+++ outline.md
@@ -0,0 +1,38 @@
+/*
+1. Display connect 4 grid on screen 
+    a. click handlers
+    b. Beautiful CSS
+        -flex boxes
+        -colors
+        -grid
+    c. No invalid moves 
+        -display ID row div
+        -click counter 
+        -once a column is full, cant add more disks 
+2. Turn based 
+    a. Keep track of disck location
+        -Cycles colors based on input counter
+    b. Properly cycle between colors 
+3. Win condition
+    a. 1 player has 4 colors in a row or diagnaly 
+    b. Program recognizes win
+        -2 variables, "or" command to allow only 1 color to activate win condition   
+
+    */
+
+/*
+1. Initalize Game 
+    a. Display board
+    b. Display instructions
+    C. Display whose turn it is 
+    d. set up click handler for column
+2.Take player Input
+    a.Determine which colum is clicked 
+    b. Determine which color is dropped 
+    c. after drop, switch active player 
+3.Check of Endgame Conditions
+    a. See if there is a 4 in a row 
+    b. See if game is tie 
+    c. Show endgame messge 
+    d. allow restart 
+*/
--- script.js
+++ script.js
@@ -2,8 +2,9 @@ let gameActive = false;
 let activePlayer = 0;
 let gameBoard = [];
 let playerColor = [];
-playerColor[1] = "blue";
-playerColor[2] = "red";
+playerColor[1] = "Blue";
+playerColor[2] = "Red";
+let dropCounts = 0 
 
 let beginGame = function () {
     if (gameActive == true) return false;
@@ -13,17 +14,20 @@ let beginGame = function () {
         gameBoard[row] = [];
         for (col = 0; col <= 6; col++) {
             gameBoard[row][col] = 0;
-        } console.log("Begin!")
+        } 
     }
-
+    let buttons = document.getElementsByClassName("drop");
+        for (i = 0; i<7; i++){
+            buttons[i].disabled = false;
+        }
+    
     drawBoard();
     activePlayer = 1;
     setUpTurn();
 }
 
 
-let drawBoard = function () {
-    console.log("Draw")
+let drawBoard = function () {                       //
     checkForWin();
     for (col = 0; col <= 6; col++) {
         for (row = 0; row <= 5; row++) {
@@ -32,14 +36,13 @@ let drawBoard = function () {
     }
 }
 
-let checkForWin = function () {
-    console.log("check")
+let checkForWin = function () { 
     for (i = 1; i <= 2; i++) {                       //Check for left to right win condition
         for (col = 0; col <= 3; col++) {
             for (row = 0; row <= 5; row++) {
                 if (gameBoard[row][col] == i) {
                     if ((gameBoard[row][col + 1] == i) && (gameBoard[row][col + 2] == i) && (gameBoard[row][col + 3] == i)) {
-                        endGame(i);
+                        endGame(i);console.log("leftRight")
                         return true;
                     }
                 }
@@ -52,7 +55,7 @@ let checkForWin = function () {
             for (row = 0; row <= 2; row++) {
                 if (gameBoard[row][col] == i) {
                     if ((gameBoard[row + 1][col] == i) && (gameBoard[row + 2][col] == i) && (gameBoard[row + 3][col] == i)) {
-                        endGame(i);
+                        endGame(i); console.log("vert")
                         return true;
                     }
                 }
@@ -64,7 +67,7 @@ let checkForWin = function () {
             for (row = 0; row <= 2; row++) {
                 if (gameBoard[row][col] == i) {
                     if ((gameBoard[row + 1][col + 1] == i) && (gameBoard[row + 2][col + 2] == i) && (gameBoard[row + 3][col + 3] == i)) {
-                        endGame(i);
+                        endGame(i); console.log("diag")
                         return true;
                     }
                 }
@@ -76,7 +79,7 @@ let checkForWin = function () {
             for (row = 3; row <= 5; row++) {
                 if (gameBoard[row][col] == i) {
                     if ((gameBoard[row - 1][col + 1] == i) && (gameBoard[row - 2][row + 2] == i) && (gameBoard[row - 3][row + 3] == i)) {
-                        endGame(i);
+                        endGame(i);console.log("diagUp")
                         return true;
                     }
                 }
@@ -87,20 +90,33 @@ let checkForWin = function () {
 
 let endGame = function(winningPlayer) {        //End Game function
     console.log("end")
+    dropCounts = 0
+    document.getElementById("game_table").disabled = true;
     gameActive = false;
     document.getElementById('game_info').innerHTML = "Crushing Victory! Player: " + winningPlayer;
+    let buttons = document.getElementsByClassName("drop");
+        for (i = 0; i<7; i++){
+            buttons[i].disabled = true;
+        }
     
 }
 
-let setUpTurn = function () {
+let setUpTurn = function () {  
+    dropCounts++
+    console.log(dropCounts)
+    if (dropCounts === 43){
+        endGame("tie!")
+    }             //Set turn for new player
     console.log("setUp")
     if (gameActive) {
         document.getElementById('game_info').innerHTML = "Current Player: Player " + activePlayer + " <span class='player" + activePlayer + "'>" + playerColor[activePlayer] + "</span>";
+        
     }
 }
 
 let drop = function (col) {                 //Add peice to the lowest possible column 
-    console.log("Drop!")
+    
+    
     for (row = 5; row >= 0; row--) {
         if (gameBoard[row][col] == 0) {
             gameBoard[row][col] = activePlayer;
@@ -120,4 +136,7 @@ let drop = function (col) {                 //Add peice to the lowest possible c
 
 
 
+
 beginGame();
+
+
